48ddffdaea8badf8340a5c525a28fe6f7374cbc0
Arya Ashari <aryaashari100@gmail.com>
2023-12-19 00:23:28 +0700

    add fitur login and logout user

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require('body-parser');
+const session = require("express-session");
+const dotenv = require("dotenv")
+
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+const userAuthMiddleware = require("./middleware/userAuthMiddleware")
+
+dotenv.config()
 
 const app = express();
 app.use(cors());
@@ -7,23 +17,77 @@ app.use(express.urlencoded({ extended: true }));
 const port = 3253;
 const expressLayouts = require("express-ejs-layouts");
 
+app.use(session({
+  secret: process.env.SECRET_KEY,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    maxAge: 604800000,
+  }
+}))
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.set("view engine", "ejs");
 app.use(expressLayouts);
 
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.render("Main_Page/index", {
-    phone_number: "+62 858 1564 8255",
-    title: "Main Page",
-    layout: "layouts/main-layout",
-  });
+
+
+app.use("/login", (req,res,next) => {
+
+  if(req.session && req.session.user) {
+    res.redirect("/");
+  } else {
+    next();
+  }
+
 });
 
 app.get("/login", (req, res) => {
   res.render("Login_Register/login_register", {
     title: "Login",
     layout: "layouts/bs-layout",
+  });
+});
+
+
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;  
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email: email
+    }
+  });
+
+  if (user && password == user.password) {
+    req.session.user = email;
+    res.redirect("/");
+  } else {
+    res.redirect("/login");
+  }
+  
+
+});
+
+
+app.use("/", (req, res, next) => {
+  if (req.session && req.session.user) {
+    next();
+  } else {
+    res.redirect('/login');
+  }
+})
+
+app.get("/", (req, res) => {
+  res.render("Main_Page/index", {
+    phone_number: "+62 858 1564 8255",
+    title: "Main Page",
+    layout: "layouts/main-layout",
   });
 });
 
@@ -87,6 +151,14 @@ app.get("/sekretaris", (req, res) => {
     layout: "layouts/bs-layout",
     phone_number: "+62 858 1564 8255",
   });
+});
+
+app.post("/logout", (req,res) => {
+
+  req.session.destroy((err) => {
+    res.redirect("/login")
+  })
+
 });
 
 app.get("/admin", (req, res) => {
