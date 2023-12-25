3be6015ac6dc1b6ff62a022df4dcadfc51ad4905
MuhammadBurhan235 <121776239+MuhammadBurhan235@users.noreply.github.com>
2023-12-25 17:26:21 +0700

    Update app.js buat for AdminDash and ListEvent

--- app.js
+++ app.js
@@ -155,12 +155,19 @@ app.get("/gallery", (req, res) => {
   });
 });
 
-app.get("/list-event", (req, res) => {
-  res.render("List_Event_Page/index", {
-    title: "List Event",
-    layout: "layouts/main-layout",
-    phone_number: "+62 858 1564 8255",
-  });
+app.get("/list-event", async (req, res) => {
+  try {
+    const events = await prisma.event.findMany();
+    res.render("List_Event_Page/index", {
+      title: "List Event",
+      layout: "layouts/main-layout",
+      phone_number: "+62 858 1564 8255",
+      events: events,
+    });
+  } catch (error) {
+    console.error("Error fetching events:", error.message);
+    throw error;
+  }
 });
 
 app.get("/my-event", async (req, res) => {
@@ -355,13 +362,40 @@ app.post("/logout", (req,res) => {
 
 });
 
-app.get("/admin", (req, res) => {
-  res.render("adminTventDash/adminSWDash.ejs", {
-    title: "Admin ",
-    layout: "layouts/main-layout",
-    phone_number: "+62 858 1564 8255",
-  });
+app.get("/admin", async (req, res) => {
+  try {
+    const events = await prisma.event.findMany();
+    res.render("adminTventDash/adminSWDash", {
+      title: "Admin",
+      layout: "layouts/main-layout",
+      phone_number: "+62 858 1564 8255",
+      events: events,
+    });
+  } catch (error) {
+    console.error("Error fetching events:", error.message);
+    throw error;
+  }
 });
+
+app.post("/verif/:eventId", async (req, res) => {
+  const eventId = parseInt(req.params.eventId);
+  const newStatus = req.body.status;
+
+  try {
+    const updatedEvent = await prisma.event.update({
+      where: { id: eventId },
+      data: {
+        status: newStatus,
+      },
+    });
+
+    res.status(401).redirect("/admin");
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
 app.listen(port, () => {
   console.log(
     `Example app listening on port ${port}, Link= http://localhost:${port}/`
