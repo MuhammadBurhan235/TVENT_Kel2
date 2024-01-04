88eb18495a6cd115eab32bf061a48d51fdaa4c16
chandra575 <kmalware88@gmail.com>
2024-01-04 10:49:51 +0700

    pagination sekre

--- app.js
+++ app.js
@@ -452,12 +452,23 @@ app.get("/profile", async (req, res) => {
 });
 
 
-app.get("/sekretaris", (req, res) => {
-  res.render("sekreDash/sekreDash.ejs", {
-    title: "Sekretaris ",
-    layout: "layouts/bs-layout",
-    phone_number: "+62 858 1564 8255",
+app.get("/sekretaris", async (req, res) => {
+  try {
+    const sekreDash = await prisma.user_registered.findMany();
+    const user = await prisma.user.findMany();
+    console.log(sekreDash)
+    console.log(user)
+      res.render("sekreDash/sekreDash.ejs", {
+      title: "Sekretaris",
+      layout: "layouts/bs-layout",
+      phone_number: "+62 858 1564 8255",
+      sekreDash: sekreDash,
+      user: user,
   });
+}catch(error){
+    console.error("Error fetching events:", error.message);
+    res.status(500).send("Internal Server Error"); // Handle the error gracefully
+  }
 });
 
 
