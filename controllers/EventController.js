933cb8304437523e197b4c6fb6c767d5da6658fc
MuhammadBurhan235 <121776239+MuhammadBurhan235@users.noreply.github.com>
2024-01-04 14:17:05 +0700

    Update EventController.js pisah jadi contr route, fungsi buat-event (css ngak kebaca)

--- controllers/EventController.js
+++ controllers/EventController.js
@@ -1,68 +1,107 @@
-const Event = require("../models/EventModel.js");
-
-const getEvents = async (req, res) => {
-  try {
-    const response = await Event.findAll();
-    res.status(200).json(response);
-  } catch {
-    console.log(error.message);
-  }
-};
-
-module.exports = getEvents;
-
-const getEventById = async (req, res) => {
-  try {
-    const response = await Event.findOne({
-      where: {
-        id: req.params.id,
-      },
-    });
-    res.status(200).json(response);
-  } catch {
-    console.log(error.message);
-  }
-};
-
-module.exports = getEventById;
+// const Event = require("../models/EventModel.js");
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
 const createEvent = async (req, res) => {
   try {
-    await Event.create(req.body);
-    res.status(201).json({ msg: "Event Created" });
-  } catch {
-    console.log(error.message);
+    const userEmail = req.session.user;
+
+    console.log(req.session.user) / console.log(req.body);
+    const eventData = {
+      // ... (rest of the code)
+    };
+    console.log(eventData);
+
+    const newEvent = await prisma.event.create({
+      data: eventData,
+    });
+    console.log("data baru berhasil diinput");
+    res.redirect("/");
+  } catch (error) {
+    console.error("ada masalah, Error: " + error);
   }
 };
 
-module.exports = createEvent;
-
-const updateEvent = async (req, res) => {
+const listEvents = async (req, res) => {
   try {
-    await Event.update(req.body, {
+    const events = await prisma.event.findMany({
       where: {
-        id: req.params.id,
+        status: "DONE",
+      },
+      select: {
+        id: true,
+        nama_event: true,
+        deskripsi_event: true,
+        kepanitiaan_mulai: true,
       },
     });
-    res.status(200).json({ msg: "Event Updated" });
-  } catch {
-    console.log(error.message);
-  }
-};
-
-module.exports = updateEvent;
-
-const deleteEvent = async (req, res) => {
-  try {
-    await Event.destroy({
-      where: {
-        id: req.params.id,
-      },
+    res.render("List_Event_Page/index", {
+      events: events,
     });
-    res.status(200).json({ msg: "Event Deleted" });
-  } catch {
-    console.log(error.message);
+  } catch (error) {
+    console.error("Error fetching events:", error.message);
+    throw error;
   }
 };
 
-module.exports = deleteEvent;
+module.exports = {
+  createEvent,
+  listEvents,
+};
+// const getEvents = async (req, res) => {
+//   try {
+//     const response = await Event.findAll();
+//     res.status(200).json(response);
+//   } catch {
+//     console.log(error.message);
+//   }
+// };
+
+// module.exports = getEvents;
+
+// const getEventById = async (req, res) => {
+//   try {
+//     const response = await Event.findOne({
+//       where: {
+//         id: req.params.id,
+//       },
+//     });
+//     res.status(200).json(response);
+//   } catch {
+//     console.log(error.message);
+//   }
+// };
+
+// module.exports = getEventById;
+
+// EventController.js
+
+// const updateEvent = async (req, res) => {
+//   try {
+//     await Event.update(req.body, {
+//       where: {
+//         id: req.params.id,
+//       },
+//     });
+//     res.status(200).json({ msg: "Event Updated" });
+//   } catch {
+//     console.log(error.message);
+//   }
+// };
+
+// module.exports = updateEvent;
+
+// const deleteEvent = async (req, res) => {
+//   try {
+//     await Event.destroy({
+//       where: {
+//         id: req.params.id,
+//       },
+//     });
+//     res.status(200).json({ msg: "Event Deleted" });
+//   } catch {
+//     console.log(error.message);
+//   }
+// };
+
+// module.exports = deleteEvent;
