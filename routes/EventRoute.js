662872fea92cbe40cbf8f54e8a81929fb818ba27
MuhammadBurhan235 <121776239+MuhammadBurhan235@users.noreply.github.com>
2024-01-04 14:18:41 +0700

    Update EventRoute.js pisah jadi contr route, fungsi buat-event (css ngak kebaca)

--- routes/EventRoute.js
+++ routes/EventRoute.js
@@ -1,18 +1,13 @@
 const express = require("express");
 const {
-  getEvents,
-  getEventById,
   createEvent,
-  updateEvent,
-  deleteEvent,
+  listEvents,
 } = require("../controllers/EventController.js");
 
 const router = express.Router();
 
-router.get("/events", getEvents);
-router.get("/events/:id", getEventById);
-router.post("/event", createEvent);
-router.patch("/event/:id", updateEvent);
-router.delete("/event/:id", deleteEvent);
+router.post("/buat-event", createEvent);
+
+router.get("/list-event", listEvents);
 
 module.exports = router;
