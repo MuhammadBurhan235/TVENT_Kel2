38dd3de1d5271af223d0706ac59debfbdb74d59e
MuhammadBurhan235 <muhammadburhan@student.telkomuniversity.ac.id>
2024-01-05 21:23:35 +0700

    login signup

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const dotenv = require("dotenv");
 const bcrypt = require("bcrypt");
 const EventRoute = require("./routes/EventRoute");
 const UserRoute = require("./routes/UserRoute");
+const AuthRoute = require("./routes/AuthRoute");
 
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
@@ -45,6 +46,7 @@ app.use(expressLayouts);
 app.use(express.static("public"));
 app.use(EventRoute);
 app.use(UserRoute);
+app.use(AuthRoute);
 
 app.use("/login", (req, res, next) => {
   if (req.session && req.session.user) {
@@ -54,87 +56,87 @@ app.use("/login", (req, res, next) => {
   }
 });
 
-app.get("/login", (req, res) => {
-  res.render("Login_Register/login_register", {
-    title: "Login",
-    layout: "layouts/bs-layout",
-  });
-});
+// app.get("/login", (req, res) => {
+//   res.render("Login_Register/login_register", {
+//     title: "Login",
+//     layout: "layouts/bs-layout",
+//   });
+// });
 
-app.post("/login", async (req, res) => {
-  const { email, password } = req.body;
-  console.log(email, password);
-  try {
-    const user = await prisma.user.findUnique({
-      where: {
-        email: email,
-      },
-    });
-    console.log(user);
+// app.post("/login", async (req, res) => {
+//   const { email, password } = req.body;
+//   console.log(email, password);
+//   try {
+//     const user = await prisma.user.findUnique({
+//       where: {
+//         email: email,
+//       },
+//     });
+//     console.log(user);
 
-    if (!user) {
-      return res.status(401).redirect("/login");
-    }
+//     if (!user) {
+//       return res.status(401).redirect("/login");
+//     }
 
-    const passwordMatch = await bcrypt.compare(password, user.password);
+//     const passwordMatch = await bcrypt.compare(password, user.password);
 
-    if (passwordMatch) {
-      req.session.user = email;
-      res.redirect("/");
-    } else {
-      res.status(401).redirect("/login");
-    }
-  } catch (error) {
-    console.error("Login error:", error);
-    res.status(500).redirect("/login");
-  }
-});
+//     if (passwordMatch) {
+//       req.session.user = email;
+//       res.redirect("/");
+//     } else {
+//       res.status(401).redirect("/login");
+//     }
+//   } catch (error) {
+//     console.error("Login error:", error);
+//     res.status(500).redirect("/login");
+//   }
+// });
 
-app.get("/signup", notLoggedInMiddleware, (req, res) => {
-  res.render("Signup/signup", {
-    title: "Sign Up",
-    layout: "layouts/bs-layout",
-  });
-});
+// app.get("/signup", notLoggedInMiddleware, (req, res) => {
+//   res.render("Signup/signup", {
+//     title: "Sign Up",
+//     layout: "layouts/bs-layout",
+//   });
+// });
 
-app.post("/signup", notLoggedInMiddleware, async (req, res) => {
-  try {
-    const {
-      email,
-      password,
-      nama_depan,
-      nama_belakang,
-      phone,
-      gender,
-      nim,
-      fakultas,
-      program_studi,
-    } = req.body;
+// app.post("/signup", notLoggedInMiddleware, async (req, res) => {
+//   try {
+//     const {
+//       email,
+//       password,
+//       nama_depan,
+//       nama_belakang,
+//       phone,
+//       gender,
+//       nim,
+//       fakultas,
+//       program_studi,
+//     } = req.body;
 
-    // Hash password sebelum menyimpan ke database
-    const hashedPassword = await bcrypt.hash(password, 10);
+//     // Hash password sebelum menyimpan ke database
+//     const hashedPassword = await bcrypt.hash(password, 10);
 
-    // Simpan data pengguna ke dalam database
-    const newUser = await prisma.user.create({
-      data: {
-        email,
-        password: hashedPassword,
-        nama_depan,
-        nama_belakang,
-        phone,
-        gender,
-        nim,
-        fakultas,
-        program_studi,
-      },
-    });
+//     // Simpan data pengguna ke dalam database
+//     const newUser = await prisma.user.create({
+//       data: {
+//         email,
+//         password: hashedPassword,
+//         nama_depan,
+//         nama_belakang,
+//         phone,
+//         gender,
+//         nim,
+//         fakultas,
+//         program_studi,
+//       },
+//     });
 
-    res.redirect("/login");
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Failed to create user" });
-  }
-});
+//     res.redirect("/login");
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: "Failed to create user" });
+//   }
+// });
 
 app.use("/", (req, res, next) => {
   if (req.session && req.session.user) {
--- routes/AuthRoute.js
+++ routes/AuthRoute.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const {
+  logIn,
+  getLogIn,
+  signUp,
+  getSignUp,
+  notLoggedInMiddleware,
+} = require("../controllers/AuthController.js");
 
-const router = express.Router;
+const router = express.Router();
 
-
-router.get("/")
-
+router.post("/login", logIn);
+router.get("/login", getLogIn);
+router.post("/signup", notLoggedInMiddleware, signUp);
+router.get("/signup", notLoggedInMiddleware, getSignUp);
 
 module.exports = router;
